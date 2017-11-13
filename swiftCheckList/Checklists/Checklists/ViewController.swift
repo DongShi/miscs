1ccb557979d24a75854937f1046a35355c5f75ca
stone <semix@mail.ustc.edu.cn>
2017-11-13 22:16:44 +0800

    feat: project init for swift study

---
+++ swiftCheckList/Checklists/Checklists/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by stone on 12/11/2017.
+//  Copyright © 2017 stone. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
