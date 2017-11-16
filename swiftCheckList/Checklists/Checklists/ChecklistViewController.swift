b3a7d0fbb94620026fd08c754947b4291d95c2ce
stone <semix@mail.ustc.edu.cn>
2017-11-16 09:23:18 +0800

    fix; fake data for row cell label

--- swiftCheckList/Checklists/Checklists/ChecklistViewController.swift
+++ swiftCheckList/Checklists/Checklists/ChecklistViewController.swift
@@ -22,11 +22,31 @@ class ChecklistViewController: UITableViewController {
     }
 
     override func tableView(_ tableview: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return 100
     }
     
     override func tableView(_ tableview: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableview.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        let label = cell.viewWithTag(1000) as! UILabel
+        switch indexPath.row % 5 {
+        case 0:
+            label.text = "Read Books"
+            break
+        case 1:
+            label.text = "Programming"
+            break
+        case 2 :
+            label.text = "Write Articles"
+            break
+        case 3:
+            label.text = "Take a rest"
+            break
+        case 4:
+            label.text = "Watch TV"
+            break
+        default:
+            label.text = "Take a rest"
+        }
         return cell
     }
 }
